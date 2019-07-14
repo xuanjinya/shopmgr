79b95a93adbc7e6bb48bfdeee33361b230899336
xuanjinya <1067998193@qq.com>
2019-07-14 21:46:33 +0800

    项目分层，创建实体类，创建Mybatis的持久层接口及映射文件

---
+++ src/main/java/TestMain.java
@@ -0,0 +1,13 @@
+import org.fkjava.dto.support.DTOHelper;
+
+/**
+ * @Author: Yaking
+ * @Date: 2019-07-14 21:41
+ * @Describe:
+ */
+public class TestMain {
+    public static void main(String[] args) throws Exception {
+        DTOHelper dto = new DTOHelper();
+        dto.createDto();
+    }
+}
---
+++ src/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package mapper;
+
+/**
+ * UserMapper 数据访问类
+ * @author xlei @qq 251425887 @tel 13352818008
+ * @Email dlei0009@163.com
+ * @date 2019-07-14 21:44:02
+ * @version 1.0
+ */
+public interface UserMapper {
+
+
+
+}
