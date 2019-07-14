84062ac36d9dd15b8bacacf8c98d31b72d437164
xuanjinya <1067998193@qq.com>
2019-07-14 22:35:41 +0800

    spring 配置文件的配置

--- src/main/java/com/imooc/shop/bean/ArticleType.java
+++ src/main/java/com/imooc/shop/bean/ArticleType.java
@@ -2,36 +2,39 @@ package com.imooc.shop.bean;
 
 /**
  * ArticleType 数据传输类
- * @author xlei @qq 251425887 @tel 13352818008
- * @Email dlei0009@163.com
- * @date 2019-07-14 21:44:02
- * @version 1.0
  */
-public class ArticleType implements java.io.Serializable{
+public class ArticleType implements java.io.Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private String code;
-	private String name;
-	private String remark;
+    private static final long serialVersionUID = 1L;
+    private String code;
+    private String name;
+    private String remark;
 
-	/** setter and getter method */
-	public void setCode(String code){
-		this.code = code;
-	}
-	public String getCode(){
-		return this.code;
-	}
-	public void setName(String name){
-		this.name = name;
-	}
-	public String getName(){
-		return this.name;
-	}
-	public void setRemark(String remark){
-		this.remark = remark;
-	}
-	public String getRemark(){
-		return this.remark;
-	}
+    /**
+     * setter and getter method
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getRemark() {
+        return this.remark;
+    }
 
 }
--- src/main/java/com/imooc/shop/repository/ArticleMapper.java
+++ src/main/java/com/imooc/shop/repository/ArticleMapper.java
@@ -2,13 +2,7 @@ package com.imooc.shop.repository;
 
 /**
  * ArticleMapper 数据访问类
- * @author xlei @qq 251425887 @tel 13352818008
- * @Email dlei0009@163.com
- * @date 2019-07-14 21:44:02
- * @version 1.0
  */
 public interface ArticleMapper {
-
-
 
 }
